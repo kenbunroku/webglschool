ddbe90111f73cf57962e8d0b3ae2c1d6caed8ffc
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2023-06-30 08:50:35 -0500

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-export default {
-  // your configuration here
-}
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      three: 'three/build/three.module.js',
+    },
+  },
+})
