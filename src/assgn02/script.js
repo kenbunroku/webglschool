d8f137e43721bf4fbbec08e2d36bd70941e3c08c
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2023-06-30 08:19:30 -0500

    Update the paths

--- src/assgn02/script.js
+++ src/assgn02/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
-import { OrbitControls } from '../lib/OrbitControls.js'
-import { EffectComposer } from '../lib/EffectComposer.js'
-import { RenderPass } from '../lib/RenderPass.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js'
 import * as dat from 'lil-gui'
 
