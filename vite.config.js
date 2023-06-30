ef320897b1af54513a51ed3946efc3819ff46f1d
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2023-06-30 08:19:41 -0500

    Remove vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['three'],
-      output: {
-        globals: {
-          three: 'THREE',
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      three: '/path/to/three.module.js', // path to your three.module.js
-    },
-  },
-})
+export default {
+  // your configuration here
+}
