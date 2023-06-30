56e09d5bc6bf56455252a1100730458688a4b4b2
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2023-06-30 12:46:37 -0500

    Change config setting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   root: 'src/',
-  publicDir: '../public/',
-  base: './',
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        assgn01: resolve(__dirname, 'src/assgn01/index.html'),
+        assgn02: resolve(__dirname, 'src/assgn02/index.html'),
+      },
+    },
   },
-}
+})
