a8263384aa4671eeeb539baef737c26ff47223fd
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2023-06-30 22:11:24 -0500

    Put back config setting

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -5,5 +6,16 @@ export default defineConfig({
   base: '/webglschool/',
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        assgn01: resolve(__dirname, 'src/assgn01/index.html'),
+        assgn02: resolve(__dirname, 'src/assgn02/index.html'),
+      },
+    },
   },
 })
