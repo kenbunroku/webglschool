5bd8782f74cf45ced032082f1fcc11b4b967f2a6
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2023-07-05 00:42:39 -0500

    Initial commitment

---
+++ src/assgn04/script.js
@@ -0,0 +1,134 @@
+import * as THREE from 'three'
+import * as dat from 'lil-gui'
+
+window.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const app = new App3()
+
+    app.init()
+    app.render()
+  },
+  false,
+)
+
+class App3 {
+  static get CAMERA_PARAM() {
+    return {
+      fovy: 45,
+      aspect: window.innerWidth / window.innerHeight,
+      near: 0.1,
+      far: 10000.0,
+      x: 10.0,
+      y: 0.0,
+      z: 2500.0,
+      lookAt: new THREE.Vector3(0.0, 0.0, 0.0),
+    }
+  }
+
+  static get RENDERER_PARAM() {
+    return {
+      clearColor: 0x000000,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }
+  }
+
+  static get DIRECTIONAL_LIGHT_PARAM() {
+    return {
+      color: 0xffffff,
+      intensity: 1.0,
+      x: 1.0,
+      y: 1.0,
+      z: 1.0,
+    }
+  }
+
+  static get AMBIENT_LIGHT_PARAM() {
+    return {
+      color: 0xffffff,
+      intensity: 0.2,
+    }
+  }
+
+  static get MATERIAL_PARAM() {
+    return { color: 0xf006eff }
+  }
+
+  constructor() {
+    this.renderer
+    this.scene
+    this.camera
+    this.controls
+    this.directionalLight
+    this.ambientLight
+    this.axesHelper
+
+    this.render = this.render.bind(this)
+
+    //Resize event
+    window.addEventListener(
+      'resize',
+      () => {
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+      },
+      false,
+    )
+  }
+
+  init() {
+    // renderer
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setClearColor(new THREE.Color(App3.RENDERER_PARAM.clearColor))
+    this.renderer.setSize(App3.RENDERER_PARAM.width, App3.RENDERER_PARAM.height)
+    const wrapper = document.querySelector('#webgl')
+    wrapper.appendChild(this.renderer.domElement)
+
+    // scene
+    this.scene = new THREE.Scene()
+
+    // camera
+    this.camera = new THREE.PerspectiveCamera(
+      App3.CAMERA_PARAM.fovy,
+      App3.CAMERA_PARAM.aspect,
+      App3.CAMERA_PARAM.near,
+      App3.CAMERA_PARAM.far,
+    )
+    this.camera.position.set(
+      App3.CAMERA_PARAM.x,
+      App3.CAMERA_PARAM.y,
+      App3.CAMERA_PARAM.z,
+    )
+    this.camera.lookAt(App3.CAMERA_PARAM.lookAt)
+
+    // directional light
+    this.directionalLight = new THREE.DirectionalLight(
+      App3.DIRECTIONAL_LIGHT_PARAM.color,
+      App3.DIRECTIONAL_LIGHT_PARAM.intensity,
+    )
+    this.directionalLight.position.set(
+      App3.DIRECTIONAL_LIGHT_PARAM.x,
+      App3.DIRECTIONAL_LIGHT_PARAM.y,
+      App3.DIRECTIONAL_LIGHT_PARAM.z,
+    )
+    this.scene.add(this.directionalLight)
+
+    // ambient light
+    this.ambientLight = new THREE.AmbientLight(
+      App3.AMBIENT_LIGHT_PARAM.color,
+      App3.AMBIENT_LIGHT_PARAM.intensity,
+    )
+    this.scene.add(this.ambientLight)
+
+    // debug
+    const gui = new dat.GUI()
+  }
+
+  render() {
+    requestAnimationFrame(this.render)
+
+    this.renderer.render(this.scene, this.camera)
+  }
+}
