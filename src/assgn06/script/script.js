6bc1b7110559ddfdf2f8fa81233fcca7fbf183bf
kenbunroku <kenbunroku524@gmail.com>
2023-08-07 10:24:39 -0500

    Remove tweak pane

--- src/assgn06/script/script.js
+++ src/assgn06/script/script.js
@@ -2,7 +2,6 @@ import { WebGLUtility } from "./webgl.js";
 import { WebGLMath } from "./math.js";
 import { WebGLGeometry } from "./geometry.js";
 import { WebGLOrbitCamera } from "./camera.js";
-// import { Pane } from "tweakpane";
 import * as dat from "lil-gui";
 
 import vertexShaderSource from "/@shaders/assgn06/main.vert";
@@ -18,27 +17,6 @@ window.addEventListener(
       app.setupLocation();
       app.start();
     });
-
-    // // Set up tweakpane
-    // const pane = new Pane();
-    // const parameter = {
-    //   culling: true,
-    //   depthTest: true,
-    //   rotation: false,
-    // };
-
-    // // Activate backface culling
-    // pane.addInput(parameter, "culling").on("change", (v) => {
-    //   app.setCulling(v.value);
-    // });
-    // // Activate depth test
-    // pane.addInput(parameter, "depthTest").on("change", (v) => {
-    //   app.setDepthTest(v.value);
-    // });
-    // // Activate rotation
-    // pane.addInput(parameter, "rotation").on("change", (v) => {
-    //   app.setRotation(v.value);
-    // });
   },
   false
 );
