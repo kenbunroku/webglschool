908c785597d1b0d2545da1b39281e4abd3b19160
kenbunroku <kenbunroku524@gmail.com>
2023-08-12 20:47:03 -0500

    Change texture parameters

--- src/assgn07/script/webgl.js
+++ src/assgn07/script/webgl.js
@@ -218,10 +218,10 @@ export class WebGLUtility {
     // ミップマップを自動生成する
     gl.generateMipmap(gl.TEXTURE_2D);
     // テクスチャパラメータを設定する
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     // 安全の為にテクスチャのバインドを解除してから返す
     gl.bindTexture(gl.TEXTURE_2D, null);
     return texture;
