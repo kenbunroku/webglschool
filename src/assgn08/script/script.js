193430ba2a7e7d71c1695a5a784183212f5889c3
kenbunroku <kenbunroku524@gmail.com>
2023-08-24 22:00:54 -0500

    Fit plane to a whole screen

--- src/assgn08/script/script.js
+++ src/assgn08/script/script.js
@@ -2,8 +2,6 @@ import { WebGLUtility } from "./webgl.js";
 import { WebGLMath } from "./math.js";
 import { WebGLGeometry } from "./geometry.js";
 import { WebGLOrbitCamera } from "./camera.js";
-import gsap from "gsap";
-import { Pane } from "tweakpane";
 
 import vertexShaderSource from "/@shaders/assgn08/render.vert";
 import fragmentShaderSource from "/@shaders/assgn08/render.frag";
@@ -64,7 +62,7 @@ class App {
     this.planeGeometry = null;
     this.offscreenPlaneGeometry = null;
 
-    /** Arry of plane VBO
+    /** Array of plane VBO
      * @type {Array.<WebGLBuffer>}
      */
     this.planeVBO = null;
@@ -97,58 +95,14 @@ class App {
      */
     this.framebufferObject = null;
 
-    /**
-     * Frame buffer texture
-     * @type {WebGLTexture}
-     */
-    this.framebufferSize = 512;
-
     /** Flag for render
      * @type {boolean}
      */
     this.isRender = false;
 
-    /** Texture visibility
-     * @type {boolean}
-     */
-    this.textureVisibility = true;
-
     // Bind to fix this
     this.resize = this.resize.bind(this);
     this.render = this.render.bind(this);
-  }
-
-  /** Set up backface culling
-   * @param {boolean} flag
-   */
-  setCulling(flag) {
-    const gl = this.gl;
-    if (gl == null) return;
-    if (flag) {
-      gl.enable(gl.CULL_FACE);
-    } else {
-      gl.disable(gl.CULL_FACE);
-    }
-  }
-
-  /** Set up depth test
-   * @param {boolean} flag
-   */
-  setDepthTest(flag) {
-    const gl = this.gl;
-    if (gl == null) return;
-    if (flag) {
-      gl.enable(gl.DEPTH_TEST);
-    } else {
-      gl.disable(gl.DEPTH_TEST);
-    }
-  }
-
-  /** Set up texture visibility
-   * @param {boolean} flag
-   */
-  setTextureVisibility(flag) {
-    this.textureVisibility = flag;
   }
 
   /** Initialization process */
@@ -157,23 +111,14 @@ class App {
     this.canvas = document.getElementById("webgl-canvas");
     this.gl = WebGLUtility.createWebGLContext(this.canvas);
 
-    const v3 = WebGLMath.Vec3;
-
     // Create instances of camera control
     const cameraOption = {
-      distance: 4.5,
+      distance: 4.0,
       min: 1.0,
       max: 10.0,
       move: 2.0,
     };
     this.camera = new WebGLOrbitCamera(this.canvas, cameraOption);
-
-    // Create frame buffer object
-    this.framebufferObject = WebGLUtility.createFramebuffer(
-      this.gl,
-      this.framebufferSize,
-      this.framebufferSize
-    );
 
     // Resize canvas
     this.resize();
@@ -306,7 +251,7 @@ class App {
       gl.getAttribLocation(this.offscreenProgram, "position"),
       gl.getAttribLocation(this.offscreenProgram, "texCoord"),
     ];
-    this.offscreenAttStride = [3, 3, 2, 4];
+    this.offscreenAttStride = [3, 2];
     this.offscreenUniLocation = {
       mvpMatrix: gl.getUniformLocation(this.offscreenProgram, "mvpMatrix"),
       normalMatrix: gl.getUniformLocation(
@@ -323,7 +268,7 @@ class App {
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.3, 0.3, 0.3, 1.0);
     gl.clearDepth(1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -338,7 +283,7 @@ class App {
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebufferObject.framebuffer);
     gl.viewport(0, 0, this.canvas.width, this.canvas.height);
 
-    gl.clearColor(1.0, 0.6, 0.9, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clearDepth(1.0);
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -375,24 +320,33 @@ class App {
     // Call requestAnimationFrame when render flag is true
     if (this.isRender) requestAnimationFrame(this.render);
 
-    const nowTime = (Date.now() - this.startTime) * 0.0001;
+    const nowTime = (Date.now() - this.startTime) * 0.001;
 
     // Set up offscreen rendering
     {
       this.setupOffscreenRendering();
 
       const v = this.camera.update();
-      const fovy = 45;
+      const fovy = Math.min(
+        2 * (180 / Math.PI) * Math.atan(2 / (2 * this.camera.distance)),
+        2 *
+          (180 / Math.PI) *
+          Math.atan(
+            4 /
+              (window.innerWidth / window.innerHeight) /
+              (2 * this.camera.distance)
+          )
+      );
+
       const aspect = window.innerWidth / window.innerHeight;
       const near = 0.1;
       const far = 10.0;
       const p = m4.perspective(fovy, aspect, near, far);
 
       const vp = m4.multiply(p, v);
-      let m = m4.translate(m4.identity(), v3.create(0.0, -1.0, 0.0));
+      let m = m4.identity();
 
       const mvp = m4.multiply(vp, m);
-      const normalMatrix = m4.transpose(m4.inverse(m));
 
       WebGLUtility.enableBuffer(
         gl,
